a827a354bbd76143d24ef7da7fb9422334ed4479
Subhranil Karmakar <97818707+Subhranil2152@users.noreply.github.com>
2024-12-22 10:08:43 +0530

    Add files via upload

---
+++ backend/config/database.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+password = quote("") 
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{password}@localhost:3306/expense_tracker'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
